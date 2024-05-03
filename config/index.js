328a0b33ea720d03567823bcd89638863f159066
zlsheepy <168760580+zlsheepy@users.noreply.github.com>
2024-05-03 19:35:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx2143198ed2c1e67e",
   appsecret: "a698ab17e6ad6f3836f62546e5f35c77",
-  template_id: "TufWRhXnGV-cZnbxVFP-H7H2r1wdlihEtMMUkiIR3jc",
+  template_id: "wK7iEzeFZYT9EpcwWnbdv9vS2CEt_nZWk838bvu3qxA",
   touser: "okUEf64E4N-CRgDMlBW13ZPtVEzI",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
